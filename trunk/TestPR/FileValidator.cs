275ca1e3ca38f1376c3c162a0ade92c79a05131d
com.chinaos.snaill <com.chinaos.snaill@ba1423b4-e665-11de-87fd-f59fab27b0b2>
2010-01-24 15:38:11 +0000

    修改测试项目

---
+++ trunk/TestPR/FileValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPR
+{
+    class FileValidator
+    {
+        public FileValidator(string path, bool bSame)
+        {
+
+        }
+
+        public void Add(string strFilename, bool bRelative)
+        {
+
+        }
+
+        public bool Validate()
+        {
+            return false;
+        }
+    }
+}
---
+++ trunk/TestPR/ReleaserTest.cs
@@ -0,0 +1,168 @@
+﻿using Jeebook.PageReleaser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestPR
+{
+    /// <summary>
+    ///This is a test class for ReleaserTest and is intended
+    ///to contain all ReleaserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ReleaserTest
+    {
+        private TestContext testContextInstance;
+
+        private void SetPath( SettingManager sm, string strFunctionName)
+        {
+            UriResolver uiRes = new UriResolver(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase, true);
+            sm.PageName = uiRes.ToAbsolute("Test\\Website\\index.html");
+            sm.OutputPath = uiRes.ToAbsolute("Test\\Result\\" + strFunctionName + "\\");
+        }
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Release
+        ///</summary>
+        [TestMethod()]
+        public void ReleaseAllTrueTest()
+        {
+            Releaser target = new Releaser(); // TODO: Initialize to an appropriate value
+            SettingManager sm = new SettingManager(); ; // TODO: Initialize to an appropriate value
+            SetPath(sm, "ReleaseAllTrueTest");
+
+            sm.IsHtmlCompress = true;
+            sm.IsHtmlGZip = true;
+
+            sm.IsJavaScriptCompress = true;
+            sm.IsJavaScriptCombine = true;
+            sm.IsJavaScriptEmbed = true;
+            sm.IsJavaScriptGZip = true;
+
+            sm.IsCssCompress = true;
+            sm.IsCssCombine = true;
+            sm.IsCssEmbed = true;
+            sm.IsCssGZip = true;
+            
+            sm.IsImageCombine = true;
+
+            sm.IgnoreRemoteFile = true;
+            sm.IgnoreParentFolder = true;
+
+            target.Release(sm);
+
+            FileValidator fv = new FileValidator(sm.OutputPath, true);
+            Assert.IsTrue(fv.Validate());
+        }
+
+        public void ReleaseAllFalseTest()
+        {
+            //
+            Releaser target = new Releaser(); 
+            SettingManager sm = new SettingManager(); 
+            SetPath(sm, "ReleaseAllFalseTest");
+
+            sm.IsHtmlCompress = true;
+            sm.IsHtmlGZip = true;
+
+            sm.IsJavaScriptCompress = true;
+            sm.IsJavaScriptCombine = true;
+            sm.IsJavaScriptEmbed = true;
+            sm.IsJavaScriptGZip = true;
+
+            sm.IsCssCompress = true;
+            sm.IsCssCombine = true;
+            sm.IsCssEmbed = true;
+            sm.IsCssGZip = true;
+
+            sm.IsImageCombine = true;
+
+            sm.IgnoreRemoteFile = true;
+            sm.IgnoreParentFolder = true;
+
+            //
+            target.Release(sm);
+
+            //
+            FileValidator fv = new FileValidator(sm.OutputPath, true);
+            Assert.IsTrue(fv.Validate());
+        }
+
+        public void ReleaseDefaultTest()
+        {
+            //
+            Releaser target = new Releaser(); 
+            SettingManager sm = new SettingManager(); 
+            SetPath(sm, "ReleaseDefaultTest");
+
+            sm.IsHtmlCompress = true;
+            sm.IsHtmlGZip = false;
+
+            sm.IsJavaScriptCompress = true;
+            sm.IsJavaScriptCombine = true;
+            sm.IsJavaScriptEmbed = false;
+            sm.IsJavaScriptGZip = false;
+
+            sm.IsCssCompress = true;
+            sm.IsCssCombine = true;
+            sm.IsCssEmbed = false;
+            sm.IsCssGZip = false;
+
+            sm.IsImageCombine = true;
+
+            sm.IgnoreRemoteFile = true;
+            sm.IgnoreParentFolder = true;
+
+            //
+            target.Release(sm);
+
+            //
+            FileValidator fv = new FileValidator(sm.OutputPath, true);
+            Assert.IsTrue(fv.Validate());
+        }
+    }
+}
